b3fe5b79e2d2d314650963ceda035fa3381f518d
Orie <orieporter@gmail.com>
2017-09-21 16:00:49 -0400

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -15,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'pizza.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,5 +45,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose.connect('mongodb://localhost/news');
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
+
+
+/*router.use('/', express.static('app', { redirect: false}));
+
+router.get('*', function (req, res,next) {
+    res.sendFile(path.resolve('app/index.html'));
+});
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/posts', function(req, res, next) {
+    Post.find(function(err, posts) {
+	if(err) {return next(err);}
+
+	res.json(posts);
+    });
+});
+
+router.post('/posts', function(req, res, next) {
+    var post = new Post(req.body);
+
+    post.save(function(err, post) {
+	if(err) {return next(err);}
+
+	res.json(post);
+    });
+});
+
+router.param('post', function(req, res, next, id) {
+    var query = Post.findById(id);
+
+    query.exec(function (err, post) {
+	if(err) {return next(err);}
+	if(!post) {return next(new Error('can\'t find post'));}
+
+	req.post = post;
+	return next();
+    });
 });
 
 module.exports = router;
