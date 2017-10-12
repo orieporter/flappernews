8e9ae1bf655ad472bf3e502f6dfdb024610ade42
orie <orieporter@gmail.com>
2017-10-12 10:29:59 -0500

    Posts state fix attempt #2

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -29,7 +29,7 @@ app.factory('posts', ['$http', function($http){
 
     o.get = function(id) {
 	return $http.get('/posts/' + id).then(function(response) {
-	    return res.data;
+	    return response.data;
 	});
     };
 
